7107fffd7042a6998ce6e060c2c7227db6d635e3
*Kim Zick (rummik) <k@9k1.us>
2021-09-10 11:18:39 -0400

    Initial

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Node.js basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+        nodejs = pkgs.nodejs-14_x;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            nodejs
+            (pkgs.yarn.override { inherit nodejs; })
+          ];
+
+          shellHook = /* sh */ ''
+            export PATH=$PATH:$PWD/node_modules/.bin
+          '';
+        };
+      });
+}
