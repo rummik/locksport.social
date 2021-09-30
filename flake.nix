60b5915981ce687215bce4e532df65e7add1d9e3
*Kim Zick (rummik) <k@9k1.us>
2021-09-30 17:54:22 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
-  description = "Node.js basic flake";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.lspkgs.url = "github:locksport/nix-flakes";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, lspkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { system = system; };
-        nodejs = pkgs.nodejs-14_x;
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodejs = pkgs.nodejs-16_x;
       in
-      {
+      rec {
         devShell = pkgs.mkShell {
           buildInputs = [
             nodejs
             (pkgs.yarn.override { inherit nodejs; })
             pkgs.go
+            lspkgs.packages.${system}.deta
           ];
 
           shellHook = /* sh */ ''
             export PATH=$PATH:$PWD/node_modules/.bin
           '';
         };
-      });
+      }
+    );
 }
