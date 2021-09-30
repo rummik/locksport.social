ff34cdd26940327265fd26207a08e0fb26bbca8a
*Kim Zick (rummik) <k@9k1.us>
2021-09-30 19:29:35 -0400

    Cleanup belt color generator, make belt base color a little lighter for better mixing

--- api/belt.ts
+++ api/belt.ts
@@ -20,12 +20,14 @@ export default async function belt(
   req: VercelRequest,
   res: VercelResponse,
 ) {
-  const svg = await readFile(__dirname + '/../svg/belt.svg', 'utf8');
   const color = parseColor(req.query.color || 'white');
   const mode = req.query.mode === 'png' ? 'png' : 'svg';
   const size = Math.min(Math.max(parseInt(req.query.size as string, 10) || 512, 16), 512);
-  const belt = svg.replace(/#[0-9a-f]{6}/g, (hex: string) =>
-    color.mix(new Color(hex)).hex()
+
+  const svg = await readFile(`${__dirname}/../svg/belt.svg`, 'utf8');
+
+  const belt = svg.replace(/#[0-9A-Fa-f]{6}/g, (hex: string) =>
+    new Color(hex).lighten(0.8).mix(color).hex()
   );
 
   switch (mode) {
