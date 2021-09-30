5a8f455bd0b054ac080451359d451d752a5af190
*Kim Zick (rummik) <k@9k1.us>
2021-09-30 18:38:36 -0400

    Add support for outputting PNG images

--- api/belt.ts
+++ api/belt.ts
@@ -4,6 +4,7 @@ import {
 } from '@vercel/node';
 
 import Color from 'color';
+import sharp from 'sharp';
 
 import { readFile } from 'fs/promises';
 
@@ -19,11 +20,25 @@ export default async function belt(
   req: VercelRequest,
   res: VercelResponse,
 ) {
-  const belt = await readFile(__dirname + '/../svg/belt.svg').then(val => val.toString());
-  const beltColor = parseColor(req.query.color);
+  const svg = await readFile(__dirname + '/../svg/belt.svg', 'utf8');
+  const color = parseColor(req.query.color || 'white');
+  const mode = req.query.mode === 'png' ? 'png' : 'svg';
+  const size = Math.min(Math.max(parseInt(req.query.size as string, 10), 16), 512);
+  const belt = svg.replace(/#[0-9a-f]{6}/g, (hex: string) =>
+    color.mix(new Color(hex)).hex()
+  );
 
-  res.setHeader('content-type', 'image/svg+xml');
-  res.status(200).send(belt.replace(/#[0-9a-f]{6}/g, (color: string) =>
-    beltColor.mix(new Color(color)).hex()
-  ));
+  switch (mode) {
+    case 'png':
+      const raster = sharp(Buffer.from(belt));
+      res.setHeader('content-type', 'image/png');
+      res.status(200).send(await raster.resize(size, size).png().toBuffer());
+      break;
+
+    case 'svg':
+    default:
+      res.setHeader('content-type', 'image/svg+xml');
+      res.status(200).send(belt);
+      break;
+  }
 }
